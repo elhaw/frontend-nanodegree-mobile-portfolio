36a50a2b9f95db120f5ee836d91cb54e9daffa43
Computer <Computer>
2017-07-15 19:38:35 +0200

    define var out of the for loop

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -539,8 +539,9 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  var elem;
   for (var i = 0; i <50; i++) {
-    var elem = document.createElement('img');
+    elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
